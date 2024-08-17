9a68afaa86c1a3d26204b8a16af9bcfb6ce9224d
David Mezler <david.mezler@outlook.com>
2024-08-17 20:23:21 +0200

    add rust embassy project

---
+++ code/mcu/src/main.rs
@@ -0,0 +1,44 @@
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_stm32::adc::{Adc, SampleTime};
+use embassy_stm32::time::Hertz;
+use embassy_stm32::Config;
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let mut config = Config::default();
+    {
+        use embassy_stm32::rcc::*;
+        config.rcc.hse = Some(Hse {
+            freq: Hertz(24_000_000),
+            mode: HseMode::Oscillator,
+        });
+        config.rcc.pll = Some(Pll {
+            source: PllSource::HSE,
+            prediv: PllPreDiv::DIV6,
+            mul: PllMul::MUL85,
+            divp: None,
+            divq: None,
+            // Main system clock at 170 MHz
+            divr: Some(PllRDiv::DIV2),
+        });
+        config.rcc.mux.adc12sel = mux::Adcsel::SYS;
+        config.rcc.sys = Sysclk::PLL1_R;
+    }
+    let mut p = embassy_stm32::init(config);
+    info!("Log");
+
+    let mut adc = Adc::new(p.ADC2);
+    adc.set_sample_time(SampleTime::CYCLES24_5);
+
+    loop {
+        let measured = adc.blocking_read(&mut p.PA6);
+        info!("measured: {}", measured);
+        Timer::after_millis(500).await;
+    }
+}
