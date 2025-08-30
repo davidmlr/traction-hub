18655d9319bcda715970d8c3a23bd6b4bdbfde3c
David Mezler <david.mezler@outlook.com>
2025-08-30 14:04:35 +0200

    code: start from new

--- code/mcu/src/fmt.rs
+++ code/mcu/src/fmt.rs
@@ -1,8 +1,5 @@
 #![allow(unused)]
 
-#[cfg(all(feature = "defmt", feature = "log"))]
-compile_error!("You may not enable both `defmt` and `log` features.");
-
 macro_rules! assert {
     ($($x:tt)*) => {
         {
@@ -134,12 +131,8 @@ macro_rules! debug {
 macro_rules! info {
     ($s:literal $(, $x:expr)* $(,)?) => {
         {
-            #[cfg(feature = "log")]
-            ::log::info!($s $(, $x)*);
             #[cfg(feature = "defmt")]
             ::defmt::info!($s $(, $x)*);
-            #[cfg(not(any(feature = "log", feature="defmt")))]
-            let _ = ($( & $x ),*);
         }
     };
 }
